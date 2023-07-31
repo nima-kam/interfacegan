85d02f46721734226d304510b06052c0ba4feb23
Nima <56191102+nima-kam@users.noreply.github.com>
2023-07-31 13:43:25 +0330

    stylegan2 generator added

--- models/stylegan2_generator.py
+++ models/stylegan2_generator.py
@@ -10,7 +10,8 @@ import numpy as np
 import torch
 
 from . import model_settings
-from .stylegan2.model import Generator as StyleGAN2GeneratorModel
+# from .stylegan2.model import Generator as StyleGAN2GeneratorModel
+from .stylegan2.stylegan2_model import StyleGAN2Generator as StyleGAN2GeneratorModel
 from .base_generator import BaseGenerator
 
 __all__ = ['StyleGAN2Generator']
@@ -37,9 +38,10 @@ class StyleGAN2Generator(BaseGenerator):
     self.check_attr('w_space_dim')
     # self.check_attr('fused_scale')
     self.model = StyleGAN2GeneratorModel(
-        size=self.resolution,
-        style_dim=self.w_space_dim,
-        # truncation_psi=self.truncation_psi,
+        resolution=self.resolution,
+        z_dim=self.w_space_dim,
+        w_dim=self.w_space_dim,
+        # trunc_psi=self.truncation_psi,
         # truncation_layers=self.truncation_layers,
         )
 
@@ -72,6 +74,8 @@ class StyleGAN2Generator(BaseGenerator):
     latent_space_type = latent_space_type.upper()
     if latent_space_type == 'Z':
       latent_codes = np.random.randn(num, self.latent_space_dim)
+      norm = np.linalg.norm(latent_codes, axis=1, keepdims=True)
+      latent_codes = latent_codes / norm * np.sqrt(self.latent_space_dim)
     elif latent_space_type == 'W':
       latent_codes = np.random.randn(num, self.w_space_dim)
     elif latent_space_type == 'WP':
@@ -150,15 +154,14 @@ class StyleGAN2Generator(BaseGenerator):
     latent_space_type = latent_space_type.upper()
     latent_codes_shape = latent_codes.shape
 
-    if self.randomize_noise:
-        noise = [None] * self.num_layers
-    else:
-        noise = [
-            getattr(self.noises, f'noise_{i}') for i in range(self.num_layers)
-        ]
-    # Generate from Z space.
-    
+    # if self.randomize_noise:
+    #     noise = [None] * self.num_layers
+    # else:
+    #     noise = [
+    #         getattr(self.noises, f'noise_{i}') for i in range(self.num_layers)
+    #     ]
 
+    # Generate from Z space. 
     if latent_space_type == 'Z':
       if not (len(latent_codes_shape) == 2 and
               latent_codes_shape[0] <= self.batch_size and
@@ -170,49 +173,66 @@ class StyleGAN2Generator(BaseGenerator):
                          f'But {latent_codes_shape} received!')
       zs = torch.from_numpy(latent_codes).type(torch.FloatTensor)
       zs = zs.to(self.run_device)
-      ws = self.style(zs)
       
-      inject_index = self.num_layers
-      if ws.ndim < 4:
-          latent = ws.unsqueeze(1).repeat(1, inject_index, 1)
-      else:
-          latent = ws
+      mapping_results = self.model.mapping(zs, impl=self.run_device)
+      ws = mapping_results['w']
+      wp = mapping_results.pop('wp')
+      
+      # inject_index = self.num_layers
+      # if ws.ndim < 4:
+      #     latent = ws.unsqueeze(1).repeat(1, inject_index, 1)
+      # else:
+      #     latent = ws
+
+      trunc_psi = 1.0 if trunc_psi is None else trunc_psi
+      trunc_layers = 0 if trunc_layers is None else trunc_layers
+      if trunc_psi < 1.0 and trunc_layers > 0:
+          w_avg = self.model.w_avg.reshape(1, -1, self.w_dim)[:, :trunc_layers]
+          wp[:, :trunc_layers] = w_avg.lerp(
+              wp[:, :trunc_layers], trunc_psi)
+
+      synthesis_results = self.model.synthesis(wp,                                               
+                                           noise_mode=self.randomize_noise,                                           
+                                           impl=self.run_device,
+                                           )
 
       results['z'] = latent_codes
       results['w'] = self.get_value(ws)
-
 
       
     # Generate from W space.
     elif latent_space_type == 'W':
 
-      inject_index = self.num_layers
-      if ws.ndim < 4:
-          latent = ws.unsqueeze(2).repeat(1,1, inject_index, 1)
-      else:
-          latent = ws
+      # inject_index = self.num_layers
+      ws = torch.from_numpy(latent_codes).type(torch.FloatTensor)
+      ws = ws.to(self.run_device)
+      wp = latent_codes.unsqueeze(1).repeat((1, self.num_outputs, 1))
       results['w'] = self.get_value(ws)
     # # Generate from W+ space.
     # elif latent_space_type == 'WP':
 
-    results['wp'] = self.get_value(latent)
+    results['wp'] = self.get_value(wp)
+    synthesis_results = self.model.synthesis(wp,                                               
+                                           noise_mode=self.randomize_noise,                                           
+                                           impl=self.run_device,
+                                           )
+    
+    # out = self.input(wp)
+    # out = self.conv1(out, wp[:, 0], noise=noise[0])
 
-    out = self.input(latent)
-    out = self.conv1(out, latent[:, 0], noise=noise[0])
+    # skip = self.to_rgb1(out, latent[:, 1])
 
-    skip = self.to_rgb1(out, latent[:, 1])
+    # i = 1
+    # for conv1, conv2, noise1, noise2, to_rgb in zip(
+    #         self.convs[::2], self.convs[1::2], noise[1::2], noise[2::2], self.to_rgbs
+    # ):
+    #     out = conv1(out, latent[:, i], noise=noise1)
+    #     out = conv2(out, latent[:, i + 1], noise=noise2)
+    #     skip = to_rgb(out, latent[:, i + 2], skip)
 
-    i = 1
-    for conv1, conv2, noise1, noise2, to_rgb in zip(
-            self.convs[::2], self.convs[1::2], noise[1::2], noise[2::2], self.to_rgbs
-    ):
-        out = conv1(out, latent[:, i], noise=noise1)
-        out = conv2(out, latent[:, i + 1], noise=noise2)
-        skip = to_rgb(out, latent[:, i + 2], skip)
+    #     i += 2
 
-        i += 2
-
-    images = skip
+    images = synthesis_results['image']
 
 
     if generate_image:
@@ -220,3 +240,36 @@ class StyleGAN2Generator(BaseGenerator):
 
 
     return results
+  
+
+class TruncationModule(nn.Module):
+  """Implements the truncation module used in StyleGAN."""
+
+  def __init__(self,
+               resolution=1024,
+               w_space_dim=512,
+               truncation_psi=0.7,
+               truncation_layers=8):
+    super().__init__()
+
+    self.num_layers = int(np.log2(resolution)) * 2 - 2
+    self.w_space_dim = w_space_dim
+    if truncation_psi is not None and truncation_layers is not None:
+      self.use_truncation = True
+    else:
+      self.use_truncation = False
+      truncation_psi = 1.0
+      truncation_layers = 0
+    self.register_buffer('w_avg', torch.zeros(w_space_dim))
+    layer_idx = np.arange(self.num_layers).reshape(1, self.num_layers, 1)
+    coefs = np.ones_like(layer_idx, dtype=np.float32)
+    coefs[layer_idx < truncation_layers] *= truncation_psi
+    self.register_buffer('truncation', torch.from_numpy(coefs))
+
+  def forward(self, w):
+    if len(w.shape) == 2:
+      w = w.view(-1, 1, self.w_space_dim).repeat(1, self.num_layers, 1)
+    if self.use_truncation:
+      w_avg = self.w_avg.view(1, 1, self.w_space_dim)
+      w = w_avg + (w - w_avg) * self.truncation
+    return w
