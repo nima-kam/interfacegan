86b6daa42c30b8b81fd9beaa815499bef7bd13e0
Nima <56191102+nima-kam@users.noreply.github.com>
2023-09-10 11:36:00 +0330

    mobile net bug fixed

--- TDDFA.py
+++ TDDFA.py
@@ -48,7 +48,7 @@ class TDDFA(object):
         )
 
         # load model, default output is dimension with length 62 = 12(pose) + 40(shape) +10(expression)
-        model = getattr(models, kvs.get('arch'))(
+        model = getattr(models.mobilenet_v1, kvs.get('arch'))(
             num_classes=kvs.get('num_params', 62),
             widen_factor=kvs.get('widen_factor', 1),
             size=self.size,
